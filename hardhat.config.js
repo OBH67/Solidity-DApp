785c1d1e353789bb883a8113a085f875d9b04acd
OBH67 <omarbalanzar113@gmail.com>
2021-05-28 12:06:03 -0700

    version solidity

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.3",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/027b1d7b171b48c0a9ecf119333b6e30",
+      accounts: ["0x738b8796df03825352167807e64c9a0f2afde8fb0b8c70e1b59162644a87dde7"]
+    }
+  }
+};
+
